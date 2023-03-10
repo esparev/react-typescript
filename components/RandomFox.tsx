40848d4495bf1eef54ddbaa5f170973ab6882412
JoseMa <esparev@hotmail.com>
2023-03-10 01:04:15 -0600

    feat: ✨ add node reference using react's hook useRef

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,7 +1,11 @@
+import { useRef } from 'react';
+
 type Props = { image: string };
 
 const RandomFox = ({ image }: Props): JSX.Element => {
-  return <img width={320} height='auto' src={image} className='rounded' />;
+  const node = useRef<HTMLImageElement>(null);
+
+  return <img ref={node} width={320} height='auto' src={image} className='rounded' />;
 };
 
 export default RandomFox;
