a8edce4bdea6893516b1604366e5b607202ca593
JoseMa <esparev@hotmail.com>
2023-03-10 01:18:33 -0600

    feat: ✨ implement lazy loading for images

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,11 +1,36 @@
-import { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 type Props = { image: string };
 
 const RandomFox = ({ image }: Props): JSX.Element => {
   const node = useRef<HTMLImageElement>(null);
+  const [src, setSrc] = useState(
+    'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4='
+  );
 
-  return <img ref={node} width={320} height='auto' src={image} className='rounded' />;
+  useEffect(() => {
+    // new observer
+    const observer = new IntersectionObserver((entries) => {
+      // on intersection
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setSrc(image);
+        }
+      });
+    });
+
+    // observe node
+    if (node.current) {
+      observer.observe(node.current);
+    }
+
+    // disconnect
+    return () => {
+      observer.disconnect();
+    };
+  }, [image]);
+
+  return <img ref={node} width={320} height='auto' src={src} className='rounded bg-gray-400' />;
 };
 
 export default RandomFox;
